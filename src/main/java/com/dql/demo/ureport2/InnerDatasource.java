7d8a7704b86578dfc2abdeae16ee6b09d17c408d
uniwaregit <guodandan@uniware.com.cn>
2021-02-03 18:12:22 +0800

    异常

--- src/main/java/com/dql/demo/ureport2/InnerDatasource.java
+++ src/main/java/com/dql/demo/ureport2/InnerDatasource.java
@@ -9,7 +9,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 
 @Component
-public class  implements BuildinDatasource {
+public class InnerDatasource  implements BuildinDatasource {
     @Autowired
     private DataSource datasource;
 
